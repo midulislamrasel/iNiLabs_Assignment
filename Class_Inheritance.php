9023cd5e9750c0f0255a6bfef89f4af1eb4b9a9d
midulislamrasel <midulraddy121@gmail.com>
2024-09-14 10:13:47 +0600

    Task 1

---
+++ Class_Inheritance.php
@@ -0,0 +1,36 @@
+<?php
+class Shape {
+    public function area() {
+        return 0;
+    }
+}
+class Circle extends Shape {
+    private $radius;
+
+    public function __construct($radius) {
+        $this->radius = $radius;
+    }
+    public function area() {
+        return pi() * pow($this->radius, 2);
+    }
+}
+class Rectangle extends Shape {
+    private $width;
+    private $height;
+
+    public function __construct($width, $height) {
+        $this->width = $width;
+        $this->height = $height;
+    }
+
+    public function area() {
+        return $this->width * $this->height;
+    }
+}
+
+// Usage
+$circle = new Circle(5);
+echo "Area of Circle: " . $circle->area() . "\n";
+
+$rectangle = new Rectangle(4, 6);
+echo "Area of Rectangle: " . $rectangle->area() . "\n";
